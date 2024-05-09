a25780913c887e1cab58e32ec9371d53da4f06e4
Amit Pandit <i2pandit@gmail.com>
2024-05-09 15:00:25 -0400

    Base model completed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.chicken_disease_classifier import logger
 from src.chicken_disease_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from src.chicken_disease_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 STAGE_NAME="Data Ingestion Stage"
 
 try:
@@ -9,6 +9,19 @@ try:
     data_ingestion.main()
     logger.info(f">>>>>>>staget {STAGE_NAME} completed<<<<<<\n\nx==========")
         
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME=" Prepare base model"
+try:
+    logger.info(f" *************")
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<")
+    prepare_base_model=PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>>> stage{STAGE_NAME} completed <<<<\n\nx=====x")
+    
 except Exception as e:
     logger.exception(e)
     raise e

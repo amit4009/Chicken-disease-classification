207cf198eeb2e34428737e06b1ce90b07e242156
Amit Pandit <i2pandit@gmail.com>
2024-05-09 16:52:42 -0400

    Callback added

---
+++ src/chicken_disease_classifier/components/prepare_callbacks.py
@@ -0,0 +1,44 @@
+import os
+import urllib.request as request
+from zipfile import ZipFile
+import tensorflow as tf
+import time
+from pathlib import Path
+
+from src.chicken_disease_classifier.entity.config_entity import PrepareCallbacksConfig
+
+
+class PrepareCallback:
+    def __init__(self, config: PrepareCallbacksConfig):
+        self.config = config
+
+
+    
+    @property
+    def _create_tb_callbacks(self):
+        timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+        tb_running_log_dir = os.path.join(
+            self.config.tensorboard_root_log_dir,
+            f"tb_logs_at_{timestamp}",
+        )
+        return tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
+    
+
+    @property
+    def _create_ckpt_callbacks(self):
+        checkpoint_model_filepath = Path(self.config.checkpoint_model_filepath).absolute()
+        if checkpoint_model_filepath.suffix != '.keras':
+            checkpoint_model_filepath = checkpoint_model_filepath.with_suffix('.keras')
+            checkpoint_model_filepath.parent.mkdir(parents=True, exist_ok=True)
+
+        return tf.keras.callbacks.ModelCheckpoint(
+        filepath=str(checkpoint_model_filepath),
+        save_best_only=True
+    )
+
+
+    def get_tb_ckpt_callbacks(self):
+        return [
+            self._create_tb_callbacks,
+            self._create_ckpt_callbacks
+        ]
